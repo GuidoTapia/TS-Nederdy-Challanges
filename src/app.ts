5b5656ddd94f1f539454f61ddf82ca03fb852919
Neo <31198370+GuidoTapia@users.noreply.github.com>
2021-06-22 21:18:12 -0500

    challenge resolved

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,59 @@ interface TemperatureSummary {
   average: number
 }
 
+interface IndexedSumary {
+  [city: string]: {
+    [time: string]: {
+      summary: TemperatureSummary
+      count: number
+    }
+  }
+}
+const temperatures: IndexedSumary = {}
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  //add here your code
+  for (const read of readings) {
+    if (temperatures[read.city] == undefined) {
+      temperatures[read.city] = {}
+    }
+    if (temperatures[read.city][read.time.toString()] == undefined) {
+      temperatures[read.city][read.time.toString()] = {
+        summary: {
+          first: read.temperature,
+          last: read.temperature,
+          high: read.temperature,
+          low: read.temperature,
+          average: read.temperature,
+        },
+        count: 1,
+      }
+    } else {
+      temperatures[read.city][read.time.toString()].summary.last =
+        read.temperature
+      if (
+        temperatures[read.city][read.time.toString()].summary.high <
+        read.temperature
+      )
+        temperatures[read.city][read.time.toString()].summary.high =
+          read.temperature
+      if (
+        temperatures[read.city][read.time.toString()].summary.low >
+        read.temperature
+      )
+        temperatures[read.city][read.time.toString()].summary.low =
+          read.temperature
+      temperatures[read.city][read.time.toString()].summary.average +=
+        read.temperature
+      temperatures[read.city][read.time.toString()].count++
+    }
+  }
+  for (const keyCity in temperatures) {
+    for (const keyTime in temperatures[keyCity]) {
+      temperatures[keyCity][keyTime].summary.average /=
+        temperatures[keyCity][keyTime].count
+    }
+  }
 }
 
 export function getTemperatureSummary(
@@ -22,5 +73,11 @@ export function getTemperatureSummary(
   city: string,
 ): TemperatureSummary | null {
   //add here your code
-  return null
+  if (temperatures[city] == undefined) {
+    return null
+  }
+  if (temperatures[city][date.toString()] == undefined) {
+    return null
+  }
+  return temperatures[city][date.toString()].summary
 }
